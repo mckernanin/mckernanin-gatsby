0d15a87c24e22ef9ac9eb7f0ee995de9e89adff1
Kevin McKernan <kevin@mckernan.in>
2019-06-20 13:31:00 -0600

    remove placeholder share images

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ module.exports = {
   siteTitleManifest: 'Kevin McKernan',
   siteUrl: 'https://mckernan.in', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteHeadline: 'Mostly tech stuff', // Headline for schema.org JSONLD
+  // siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  // favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: '', // Your site description
   author: 'LekoArts', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+  // siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@kevinmckernan', // Twitter Username - Optional
